d30147fea95a3714ec2ef6a6a55d780cb8dc959e
victorromeo <victorromeo@github.com>
2020-01-23 15:41:08 +0000

    Cleanup after build

--- src/arm/build.sh
+++ src/arm/build.sh
@@ -4,5 +4,10 @@
 # cp usbhidtool ../../bin/linux/usbhidtool
 # rm -f usbhidtool
 
-cmake . -DCMAKE_TOOLCHAIN_FILE=Toolchain-rpi.cmake
-make
+cmake . -DCMAKE_TOOLCHAIN_FILE=Toolchain-rpi.cmake && make && cp usbhidtool ../../bin/arm/usbhidtool
+rm -f usbhidtool
+rm -rf CMakeFiles
+rm -f CMakeCache.txt
+rm -f cmake_install.cmake
+rm -f Makefile
+
