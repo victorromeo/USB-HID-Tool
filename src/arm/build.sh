a5e8af30e9d5014d94fe27d593dc0e91245f2cfa
victorromeo <victorromeo@github.com>
2020-01-23 01:15:41 +0000

    Init ARM

---
+++ src/arm/build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# gcc -Wall -std=c99 -I/usr/include/libusb-1.0 -I.. -o usbhidtool ../main.c hid.c -lusb-1.0 -lrt -lpthread
+# cp usbhidtool ../../bin/linux/usbhidtool
+# rm -f usbhidtool
+
+cmake . -DCMAKE_TOOLCHAIN_FILE=Toolchain-rpi.cmake
+make
